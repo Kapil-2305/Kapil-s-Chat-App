06bbe937fdb4bc092e1ff70dc300a97d3a9876a0
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-19 19:52:54 +0530

    Add black background to DashboardLayout

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const DashboardLayout = () => {
+    const theme = useTheme();
 
-  return (
-    <>
-      Dashboard Layout
-      <Outlet />
-    </>
-  );
+    console.log(theme);
+
+    return (
+        <>
+            <Box sx={{background: "black", height: "100vh", width: 100}}>
+
+            </Box>
+            <Outlet />
+        </>
+    );
 };
 
 export default DashboardLayout;
