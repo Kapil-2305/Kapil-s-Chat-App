8a8222b49dbe5ebf63fc4f7337d1e98a42bb49ea
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-28 23:59:16 +0530

    Add primary color box to dashboard layout

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -9,7 +9,13 @@ const DashboardLayout = () => {
     return (
         <>
             <Box sx={{background: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width: 100}}>
+                <Box sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    height: 64,
+                    width: 64,
+                }}>
 
+                </Box>
             </Box>
             <Outlet />
         </>
