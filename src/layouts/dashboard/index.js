3b81147a39be6f152e4cc95b08320ee6d0720f73
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-30 23:42:01 +0530

    Update dashboard layout with logo

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+
+import Logo from "../../assets/Images/logo.ico"
 
 const DashboardLayout = () => {
     const theme = useTheme();
@@ -9,14 +11,16 @@ const DashboardLayout = () => {
     return (
         <>
             <Box sx={{background: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width: 100}}>
-                <Box sx={{
-                    backgroundColor: theme.palette.primary.main,
-                    height: 64,
-                    width: 64,
-                    borderRadius: 2.5,
-                }}>
-
-                </Box>
+                <Stack direction="column" sx={{width: "100%"}}>
+                    <Box sx={{
+                        backgroundColor: theme.palette.primary.main,
+                        height: 64,
+                        width: 64,
+                        borderRadius: 1.5,
+                    }}>
+                        <img src={Logo} alt="Chat App Logo"/>
+                    </Box>
+                </Stack>
             </Box>
             <Outlet />
         </>
