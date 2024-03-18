02f85e7f82de5573dabd5ac4ab2ef828189906de
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-03-19 02:16:37 +0530

    Add Conversation component and integrate it into GeneralApp

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Chats from "./Chats";
+import { Stack, Box } from "@mui/material";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
     return (
-        <>
+        <Stack direction={"row"} sx={{width: "100%"}}>
+            {/* Chats */}
             <Chats />
-        </>
+
+            {/* Conversation */}
+            <Box sx={{height: "100%", width: "calc(100vw - 416px)", backgroundColor: "#fff"}}>
+                <Conversation />
+            </Box>
+        </Stack>
     );
 };
 
