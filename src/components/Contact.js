de8934acb3ee4571ee2699279f0589b4316a46e7
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-28 00:45:36 +0530

    feat: Add BlockDialog and DeleteChatDialog components for blocking contacts and deleting chats

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,49 +36,49 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 });
 
 const BlockDialog = ({ open, handleClose }) => {
-  return (
-    <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-    >
-        <DialogTitle>Block this contact</DialogTitle>
-        <DialogContent>
-            <DialogContentText id="alert-dialog-slide-description">
-                Are you sure you want to block this Contact?
-            </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleClose}>Yes</Button>
-        </DialogActions>
-    </Dialog>
-  );
+    return (
+        <Dialog
+            open={open}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+        >
+            <DialogTitle>Block this contact</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    Are you sure you want to block this Contact?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleClose}>Yes</Button>
+            </DialogActions>
+        </Dialog>
+    );
 };
 
 const DeleteChatDialog = ({ open, handleClose }) => {
-  return (
-    <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-    >
-        <DialogTitle>Delete this chat</DialogTitle>
-        <DialogContent>
-            <DialogContentText id="alert-dialog-slide-description">
-                Are you sure you want to delete this chat?
-            </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleClose}>Yes</Button>
-        </DialogActions>
-    </Dialog>
-  );
+    return (
+        <Dialog
+            open={open}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+        >
+            <DialogTitle>Delete this chat</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    Are you sure you want to delete this chat?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleClose}>Yes</Button>
+            </DialogActions>
+        </Dialog>
+    );
 };
 
 const Contact = () => {
