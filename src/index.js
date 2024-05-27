85696a4b21090ff6db5e5f5f899352f6bb7b399b
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-27 15:26:16 +0530

    feat: Update axios dependency to version 1.7.2

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ import reportWebVitals from "./reportWebVitals";
 
 // contexts
 import SettingsProvider from "./contexts/SettingsContext";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <SettingsProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SettingsProvider>
-    </HelmetProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <HelmetProvider>
+            <ReduxProvider store={store}>
+                <SettingsProvider>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </SettingsProvider>
+            </ReduxProvider>
+        </HelmetProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
