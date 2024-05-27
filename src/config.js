44709112012caf64ae82ba8566cdd43a8e300c16
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-27 18:27:00 +0530

    feat: Add socket.io-client, axios, and app slice for real-time communication, API integration, and sidebar changes

--- src/config.js
+++ src/config.js
@@ -4,6 +4,11 @@ import { enUS, frFR, zhCN, viVN, arSD } from '@mui/material/locale';
 // routes
 import { PATH_DASHBOARD } from "./routes/paths";
 
+export const BASE_URL = "https://api.chat.codingmonk.in/";
+
+export const S3_BUCKET_NAME = 'codingmonk';
+export const AWS_S3_REGION = 'ap-south-1'; // eg. ap-south-1
+
 export const defaultSettings = {
   themeMode: "light",
   themeDirection: "ltr",
