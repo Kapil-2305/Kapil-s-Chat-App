67c4cea69f21d1f6f3545d6f24e5aee0e139bad4
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-28 15:03:23 +0530

    feat: Add ChatElement component for displaying chat information

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -3,57 +3,9 @@ import { Box, Stack, Typography, IconButton, Button, Divider, Avatar, Badge } fr
 import { styled, useTheme } from "@mui/material/styles"
 import { CircleDashed, MagnifyingGlass, ArchiveBox } from "phosphor-react";
 import { Search, SearchIconWrapper, StyledInputBase } from "../../components/Search";
-import { faker } from "@faker-js/faker";
 import { ChatList } from "../../data";
 import { SimpleBarStyle } from "../../components/Scrollbar"
-import StyledBadge from "../../components/StyledBadge";
-
-const ChatElement = ({id, name, img, msg, time, unread, online}) => {
-    const theme = useTheme();
-    return (
-        <Box sx={{
-            width: "100%",
-            borderRadius: 1,
-            backgroundColor: theme.palette.mode === "light" ? "#FFF" : theme.palette.background.paper,
-        }}
-        p={2}>
-            <Stack direction="row" alignItems="center" justifyContent="space-between">
-                <Stack direction="row" spacing={2}>
-                    {online ? (
-                        <StyledBadge 
-                            overlap="circular"
-                            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                            variant="dot"
-                        >
-                            <Avatar src={faker.image.avatar()} />
-                        </StyledBadge>
-                    ) : (
-                        <Avatar src={faker.image.avatar()} />
-                    )}
-
-                    <Stack spacing={0.3}>
-                        <Typography variant="subtitle2">
-                            {name}
-                        </Typography>
-                        <Typography variant="caption">
-                            {msg}
-                        </Typography>
-                    </Stack>
-                </Stack>
-
-                <Stack spacing={2} alignItems="center">
-                    <Typography sx={{fontWeight: 600}} variant="caption">
-                        {time}
-                    </Typography>
-                    <Badge color="primary" badgeContent={unread}>
-
-                    </Badge>
-                </Stack>
-            </Stack>
-        </Box>
-    );
-
-}
+import ChatElement from "../../components/ChatElement";
 
 const Chats = () => {
     const theme = useTheme();
