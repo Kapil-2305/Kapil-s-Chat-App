503181fd581069283bfe57e54409d9fb91534240
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-28 11:46:20 +0530

    feat: Add AuthLayout component for authentication pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import DashboardLayout from "../layouts/dashboard";
 // config
 import { DEFAULT_PATH } from "../config";
 import LoadingScreen from "../components/LoadingScreen";
+import AuthLayout from "../layouts/auth";
 
 const Loadable = (Component) => (props) => {
   return (
@@ -18,6 +19,13 @@ const Loadable = (Component) => (props) => {
 
 export default function Router() {
   return useRoutes([
+    {
+        path: "/auth",
+        element: <AuthLayout />,
+        children: [
+            {path: "login", element: <LoginPage />},
+        ]
+    },
     {
       path: "/",
       element: <DashboardLayout />,
@@ -34,10 +42,7 @@ export default function Router() {
   ]);
 }
 
-const GeneralApp = Loadable(
-  lazy(() => import("../pages/dashboard/GeneralApp")),
-);
-const Settings = Loadable(
-    lazy(() => import("../pages/dashboard/Settings"))
-);
+const GeneralApp = Loadable(lazy(() => import("../pages/dashboard/GeneralApp")));
+const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
+const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
