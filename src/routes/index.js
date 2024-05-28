96dffaeca925852fd679ab14b4acb2592d35d184
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-28 18:51:37 +0530

    feat: Add CallPage component for making calls

--- src/routes/index.js
+++ src/routes/index.js
@@ -38,6 +38,7 @@ export default function Router() {
         { path: "app", element: <GeneralApp /> },
         { path: "settings", element: <Settings />},
         { path: "group", element: <GroupPage />},
+        { path: "call", element: <CallPage />},
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
@@ -49,6 +50,7 @@ export default function Router() {
 const GeneralApp = Loadable(lazy(() => import("../pages/dashboard/GeneralApp")));
 const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 const GroupPage = Loadable(lazy(() => import("../pages/dashboard/Group")));
+const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")));
 
 const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
 const RegisterPage = Loadable(lazy(() => import("../pages/auth/Register")));
