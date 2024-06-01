9737c8086b3c6772e0f2dba41ed97e87d6b56d88
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-06-02 00:13:58 +0530

    chore: Update server.js to connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,11 +1,44 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({path: './.env'});
+
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
 
 const http = require('http');
 
 const server = http.createServer(app);
 
+const DB = process.env.DB_URL.replace("<password>", process.env.DB_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('DB connection successful');
+})
+.catch(err => {
+    console.log(err);
+});
+
 const port = process.env.PORT || 8000;
 
 server.listen(3000, () => {
-    console.log('Server is listening on port ${port');
+    console.log(`Server is listening on port ${port}`);
 });
