c27cc110c0b2ca5ac4df8537b4b1ff89bd7b16fd
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-07-06 23:51:51 +0530

    chore: Refactor auth.js slice and login functionality

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -6,37 +6,37 @@ import { showSnackbar } from "./app";
 // ----------------------------------------------------------------------
 
 const initialState = {
-  isLoggedIn: false,
-  token: "",
-  isLoading: false,
-  user: null,
-  user_id: null,
-  email: "",
-  error: false,
+    isLoggedIn: false,
+    token: "",
+    isLoading: false,
+    user: null,
+    user_id: null,
+    email: "",
+    error: false,
 };
 
 const slice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    updateIsLoading(state, action) {
-      state.error = action.payload.error;
-      state.isLoading = action.payload.isLoading;
+    name: "auth",
+    initialState,
+    reducers: {
+        updateIsLoading(state, action) {
+            state.error = action.payload.error;
+            state.isLoading = action.payload.isLoading;
+        },
+        logIn(state, action) {
+            state.isLoggedIn = action.payload.isLoggedIn;
+            state.token = action.payload.token;
+            state.user_id = action.payload.user_id;
+        },
+        signOut(state, action) {
+            state.isLoggedIn = false;
+            state.token = "";
+            state.user_id = null;
+        },
+        updateRegisterEmail(state, action) {
+            state.email = action.payload.email;
+        },
     },
-    logIn(state, action) {
-      state.isLoggedIn = action.payload.isLoggedIn;
-      state.token = action.payload.token;
-      state.user_id = action.payload.user_id;
-    },
-    signOut(state, action) {
-      state.isLoggedIn = false;
-      state.token = "";
-      state.user_id = null;
-    },
-    updateRegisterEmail(state, action) {
-      state.email = action.payload.email;
-    },
-  },
 });
 
 // Reducer
@@ -120,48 +120,46 @@ export function ForgotPassword(formValues) {
 }
 
 export function LoginUser(formValues) {
-  return async (dispatch, getState) => {
-    // Make API call here
+    return async (dispatch, getState) => {
+        // Make API call here
 
-    dispatch(slice.actions.updateIsLoading({ isLoading: true, error: false }));
+        dispatch(slice.actions.updateIsLoading({ isLoading: true, error: false }));
 
-    await axios
-      .post(
-        "/auth/login",
-        {
-          ...formValues,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then(function (response) {
-        console.log(response);
-        dispatch(
-          slice.actions.logIn({
-            isLoggedIn: true,
-            token: response.data.token,
-            user_id: response.data.user_id,
-          })
-        );
-        window.localStorage.setItem("user_id", response.data.user_id);
-        dispatch(
-          showSnackbar({ severity: "success", message: response.data.message })
-        );
-        dispatch(
-          slice.actions.updateIsLoading({ isLoading: false, error: false })
-        );
-      })
-      .catch(function (error) {
-        console.log(error);
-        dispatch(showSnackbar({ severity: "error", message: error.message }));
-        dispatch(
-          slice.actions.updateIsLoading({ isLoading: false, error: true })
-        );
-      });
-  };
+        await axios.post("/auth/login",
+            {
+                ...formValues,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+        .then(function (response) {
+            console.log(response);
+            dispatch(
+                slice.actions.logIn({
+                    isLoggedIn: true,
+                    token: response.data.token,
+                    user_id: response.data.user_id,
+                })
+            );
+            window.localStorage.setItem("user_id", response.data.user_id);
+            dispatch(
+                showSnackbar({ severity: "success", message: response.data.message })
+            );
+            dispatch(
+                slice.actions.updateIsLoading({ isLoading: false, error: false })
+            );
+        })
+        .catch(function (error) {
+            console.log(error);
+            dispatch(showSnackbar({ severity: "error", message: error.message }));
+            dispatch(
+                slice.actions.updateIsLoading({ isLoading: false, error: true })
+            );
+        });
+    };
 }
 
 export function LogoutUser() {
