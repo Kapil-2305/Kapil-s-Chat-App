ea8dc4e98d0e7504d9ead3a4c25659f24b04f95e
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-07-12 15:12:23 +0530

    chore: Update App.js to include Snackbar component for displaying notifications

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import ThemeProvider from './theme';
 // components
 import ThemeSettings from './components/settings';
 
+import { Snackbar } from "@mui/material";
+
+const Alert = React.forwardRef((props, ref) => (
+    <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+));
+
 function App() {
-  return (
-    <ThemeProvider>
-      <ThemeSettings>
-        {" "}
-        <Router />{" "}
-      </ThemeSettings>
-    </ThemeProvider>
-  );
+    return (
+        <>
+            <ThemeProvider>
+                <ThemeSettings>
+                    {" "}
+                    <Router />{" "}
+                </ThemeSettings>
+            </ThemeProvider>
+
+            <Snackbar anchorOrigin={{vertical: "bottom", horizontal: "center"}} open={open} autoHideDuration={4000} key={horizontal + vertical} onClose={()=>{}}></Snackbar>
+        </>
+    );
 }
 
 export default App;
