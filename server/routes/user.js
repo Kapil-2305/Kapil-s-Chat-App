aa1c1752d144806944ecbaf7bc79f8d5628fba15
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-07-12 15:24:36 +0530

    chore: Enable getUsers endpoint to retrieve remaining users

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,7 +12,7 @@ const authController = require("../controllers/auth");
 // router.get("/get-me", authController.protect, userController.getMe);
 router.patch("/update-me", authController.protect, userController.updateMe);
 // router.get("/get-all-verified-users", authController.protect, userController.getAllVerifiedUsers);
-// router.get("/get-users", authController.protect, userController.getUsers);
+router.get("/get-users", authController.protect, userController.getUsers);
 // router.get("/get-requests", authController.protect, userController.getRequests);
 // router.get("/get-friends", authController.protect, userController.getFriends);
 
