46e2814541fedc73d15109499acd523e1da0b1f6
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-07-13 23:12:24 +0530

    refactor: Fix typo in import statement for UserElement component

--- src/sections/dashboard/Friends.js
+++ src/sections/dashboard/Friends.js
@@ -10,7 +10,7 @@ import {
     FriendElement,
     FriendRequestElement,
     UserElement,
-} from "../../components/UserEleement";
+} from "../../components/UserElement";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
