11b1903e6472cc9c5c5b439e764314196ce3bbe8
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-07-13 23:06:47 +0530

    refactor: Update conversation slice to handle direct chat functionality

--- src/redux/slices/conversation.js
+++ src/redux/slices/conversation.js
@@ -111,36 +111,35 @@ export default slice.reducer;
 // ----------------------------------------------------------------------
 
 export const FetchDirectConversations = ({ conversations }) => {
-  return async (dispatch, getState) => {
-    dispatch(slice.actions.fetchDirectConversations({ conversations }));
-  };
+    return async (dispatch, getState) => {
+        dispatch(slice.actions.fetchDirectConversations({ conversations }));
+    };
 };
 export const AddDirectConversation = ({ conversation }) => {
-  return async (dispatch, getState) => {
-    dispatch(slice.actions.addDirectConversation({ conversation }));
-  };
+    return async (dispatch, getState) => {
+        dispatch(slice.actions.addDirectConversation({ conversation }));
+    };
 };
 export const UpdateDirectConversation = ({ conversation }) => {
-  return async (dispatch, getState) => {
-    dispatch(slice.actions.updateDirectConversation({ conversation }));
-  };
+    return async (dispatch, getState) => {
+        dispatch(slice.actions.updateDirectConversation({ conversation }));
+    };
 };
 
 export const SetCurrentConversation = (current_conversation) => {
-  return async (dispatch, getState) => {
-    dispatch(slice.actions.setCurrentConversation(current_conversation));
-  };
+    return async (dispatch, getState) => {
+        dispatch(slice.actions.setCurrentConversation(current_conversation));
+    };
 };
 
-
 export const FetchCurrentMessages = ({messages}) => {
-  return async(dispatch, getState) => {
-    dispatch(slice.actions.fetchCurrentMessages({messages}));
-  }
+    return async(dispatch, getState) => {
+        dispatch(slice.actions.fetchCurrentMessages({messages}));
+    }
 }
 
 export const AddDirectMessage = (message) => {
-  return async (dispatch, getState) => {
-    dispatch(slice.actions.addDirectMessage({message}));
-  }
+    return async (dispatch, getState) => {
+        dispatch(slice.actions.addDirectMessage({message}));
+    }
 }
