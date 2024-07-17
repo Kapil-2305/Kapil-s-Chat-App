5db3d094dee52ae8d3b02afb2e8aa1f45ece809a
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-07-17 16:52:13 +0530

    refactor: Move Profile.js to the Settings folder in the dashboard

--- src/pages/dashboard/Settings/Profile.js
+++ src/pages/dashboard/Settings/Profile.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import { Box, IconButton, Stack, Typography } from "@mui/material";
 import { CaretLeft } from "phosphor-react";
-import ProfileForm from "../../sections/dashboard/settings/ProfileForm";
+import ProfileForm from "../../../sections/dashboard/settings/ProfileForm";
 import { useDispatch } from "react-redux";
-import { FetchUserProfile } from "../../redux/slices/app";
+import { FetchUserProfile } from "../../../redux/slices/app";
 
 const Profile = () => {
     const dispatch = useDispatch();
-    
+
     useEffect(() => {
         dispatch(FetchUserProfile());
     }, []);
@@ -22,25 +22,25 @@ const Profile = () => {
                         height: "100vh",
                         width: 320,
                         backgroundColor: (theme) =>
-                        theme.palette.mode === "light"
-                            ? "#F8FAFF"
-                            : theme.palette.background,
+                            theme.palette.mode === "light"
+                                ? "#F8FAFF"
+                                : theme.palette.background,
                         boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
                     }}
                 >
-                <Stack p={4} spacing={5}>
-                    {/* Header */}
-                    <Stack direction="row" alignItems={"center"} spacing={3}>
-                        <IconButton>
-                            <CaretLeft size={24} color={"#4B4B4B"} />
-                        </IconButton>
+                    <Stack p={4} spacing={5}>
+                        {/* Header */}
+                        <Stack direction="row" alignItems={"center"} spacing={3}>
+                            <IconButton>
+                                <CaretLeft size={24} color={"#4B4B4B"} />
+                            </IconButton>
 
-                        <Typography variant="h5">Profile</Typography>
+                            <Typography variant="h5">Profile</Typography>
+                        </Stack>
+
+                        {/* Profile Edit Form */}
+                        <ProfileForm />
                     </Stack>
-
-                    {/* Profile Edit Form */}
-                    <ProfileForm />
-                </Stack>
                 </Box>
 
                 {/* Right Pane */}
@@ -49,9 +49,9 @@ const Profile = () => {
                         height: "100%",
                         width: "calc(100vw - 420px )",
                         backgroundColor: (theme) =>
-                        theme.palette.mode === "light"
-                            ? "#FFF"
-                            : theme.palette.background.paper,
+                            theme.palette.mode === "light"
+                                ? "#FFF"
+                                : theme.palette.background.paper,
                         borderBottom: "6px solid #0162C4",
                     }}
                 ></Box>
