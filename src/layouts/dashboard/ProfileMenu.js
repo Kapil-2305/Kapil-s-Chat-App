c93cf256efc941aa65e020021d7bc93a668348eb
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-07-18 01:11:18 +0530

    refactor: Optimize ProfileMenu component in the dashboard

--- src/layouts/dashboard/ProfileMenu.js
+++ src/layouts/dashboard/ProfileMenu.js
@@ -4,30 +4,16 @@ import { Avatar, Box, Fade, Menu, MenuItem, Stack } from "@mui/material";
 import { faker } from "@faker-js/faker";
 
 import { Profile_Menu } from "../../data";
-// import { useDispatch, useSelector } from "react-redux";
-// import { LogoutUser } from "../../redux/slices/auth";
-// import { socket } from "../../socket";
+import { useDispatch, useSelector } from "react-redux";
+import { LogoutUser } from "../../redux/slices/auth";
+import { socket } from "../../socket";
 import { useNavigate } from "react-router-dom";
-// import { AWS_S3_REGION, S3_BUCKET_NAME } from "../../config";
-
-const getMenuPath = (index) => {
-    switch (index) {
-        case 0:
-            return "/profile";
-        case 1:
-            return "/settings";
-        case 2:
-            // Todo => update token & setAuth = false
-            return "/auth/login";
-        default:
-            break;
-    }
-}
+import { AWS_S3_REGION, S3_BUCKET_NAME } from "../../config";
 
 const ProfileMenu = () => {
-    // const {user} = useSelector((state) => state.app);
+    const { user } = useSelector((state) => state.app);
     const navigate = useNavigate();
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const [anchorEl, setAnchorEl] = React.useState(null);
     const openMenu = Boolean(anchorEl);
     const handleClick = (event) => {
@@ -37,63 +23,71 @@ const ProfileMenu = () => {
         setAnchorEl(null);
     };
 
-    // const user_id = window.localStorage.getItem("user_id");
+    const user_id = window.localStorage.getItem("user_id");
 
-    // const user_name = user?.firstName;
-    //   const user_img = `https://${S3_BUCKET_NAME}.s3.${AWS_S3_REGION}.amazonaws.com/${user?.avatar}`;
+    const user_name = user?.firstName;
+    const user_img = `https://${S3_BUCKET_NAME}.s3.${AWS_S3_REGION}.amazonaws.com/${user?.avatar}`;
 
     return (
         <>
-        <Avatar
-            id="profile-positioned-button"
-            aria-controls={openMenu ? "profile-positioned-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={openMenu ? "true" : undefined}
-            // alt={user_name}
-            // src={user_img}
-            onClick={handleClick}
-        />
-        <Menu
-            MenuListProps={{
-                "aria-labelledby": "fade-button",
-            }}
-            TransitionComponent={Fade}
-            id="profile-positioned-menu"
-            aria-labelledby="profile-positioned-button"
-            anchorEl={anchorEl}
-            open={openMenu}
-            onClose={handleClose}
-            anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "right",
-            }}
-            transformOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-            }}
-        >
-            <Box p={1}>
-                <Stack spacing={1}>
-                    {Profile_Menu.map((el, idx) => (
-                        <MenuItem key={idx}>
-                            <Stack
-                                onClick={() => {
-                                    navigate(getMenuPath(idx));
-                                    handleClose();
-                                }}
-                                sx={{ width: 100 }}
-                                direction="row"
-                                alignItems={"center"}
-                                justifyContent="space-between"
-                            >
-                                <span>{el.title}</span>
-                                {el.icon}
-                            </Stack>{" "}
-                        </MenuItem>
-                    ))}
-                </Stack>
-            </Box>
-        </Menu>
+            <Avatar
+                id="profile-positioned-button"
+                aria-controls={openMenu ? "profile-positioned-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={openMenu ? "true" : undefined}
+                alt={user_name}
+                src={user_img}
+                onClick={handleClick}
+            />
+            <Menu
+                MenuListProps={{
+                    "aria-labelledby": "fade-button",
+                }}
+                TransitionComponent={Fade}
+                id="profile-positioned-menu"
+                aria-labelledby="profile-positioned-button"
+                anchorEl={anchorEl}
+                open={openMenu}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                }}
+                transformOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                }}
+            >
+                <Box p={1}>
+                    <Stack spacing={1}>
+                        {Profile_Menu.map((el, idx) => (
+                            <MenuItem onClick={handleClose}>
+                                <Stack
+                                    onClick={() => {
+                                        if (idx === 0) {
+                                            navigate("/profile");
+                                        }
+                                        else if (idx === 1) {
+                                            navigate("/settings");
+                                        }
+                                        else {
+                                            dispatch(LogoutUser());
+                                            socket.emit("end", { user_id });
+                                        }
+                                    }}
+                                    sx={{ width: 100 }}
+                                    direction="row"
+                                    alignItems={"center"}
+                                    justifyContent="space-between"
+                                >
+                                    <span>{el.title}</span>
+                                    {el.icon}
+                                </Stack>{" "}
+                            </MenuItem>
+                        ))}
+                    </Stack>
+                </Box>
+            </Menu>
         </>
     );
 };
