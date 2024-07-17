de482ed19c3524ff5208f07d6db7a58482fb476f
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-07-17 16:52:55 +0530

    refactor: Update Router component to use lazy loading for route components

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,54 +10,62 @@ import LoadingScreen from "../components/LoadingScreen";
 import AuthLayout from "../layouts/auth";
 
 const Loadable = (Component) => (props) => {
-  return (
-    <Suspense fallback={<LoadingScreen />}>
-      <Component {...props} />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<LoadingScreen />}>
+            <Component {...props} />
+        </Suspense>
+    );
 };
 
 export default function Router() {
-  return useRoutes([
-    {
-        path: "/auth",
-        element: <AuthLayout />,
-        children: [
-            {path: "login", element: <LoginPage />},
-            {path: "register", element: <RegisterPage />},
-            {path: "reset-password", element: <ResetPasswordPage />},
-            {path: "new-password", element: <NewPasswordPage />},
-            {path: "verify", element: <VerifyPage /> },
-        ]
-    },
-    {
-      path: "/",
-      element: <DashboardLayout />,
-      children: [
-        { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
-        { path: "app", element: <GeneralApp /> },
-        { path: "settings", element: <Settings />},
-        { path: "group", element: <GroupPage />},
-        { path: "call", element: <CallPage />},
-        { path: "profile", element: <ProfilePage />},
-        { path: "404", element: <Page404 /> },
+    return useRoutes([
+        {
+            path: "/auth",
+            element: <AuthLayout />,
+            children: [
+                { path: "login", element: <LoginPage /> },
+                { path: "register", element: <RegisterPage /> },
+                { path: "reset-password", element: <ResetPasswordPage /> },
+                { path: "new-password", element: <NewPasswordPage /> },
+                { path: "verify", element: <VerifyPage /> },
+            ],
+        },
+        {
+            path: "/",
+            element: <DashboardLayout />,
+            children: [
+                { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
+                { path: "app", element: <GeneralApp /> },
+                { path: "settings", element: <Settings /> },
+                { path: "group", element: <GroupPage /> },
+                { path: "call", element: <CallPage /> },
+                { path: "profile", element: <Profile /> },
+                { path: "404", element: <Page404 /> },
+                { path: "*", element: <Navigate to="/404" replace /> },
+            ],
+        },
         { path: "*", element: <Navigate to="/404" replace /> },
-      ],
-    },
-    { path: "*", element: <Navigate to="/404" replace /> },
-  ]);
+    ]);
 }
 
-const GeneralApp = Loadable(lazy(() => import("../pages/dashboard/GeneralApp")));
+const GeneralApp = Loadable(
+    lazy(() => import("../pages/dashboard/GeneralApp"))
+);
 const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 const GroupPage = Loadable(lazy(() => import("../pages/dashboard/Group")));
 const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")));
-const ProfilePage = Loadable(lazy(() => import("../pages/dashboard/Profile")));
+const Profile = Loadable(
+    lazy(() => import("../pages/dashboard/Settings/Profile"))
+);
 
 const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
 const RegisterPage = Loadable(lazy(() => import("../pages/auth/Register")));
-const ResetPasswordPage = Loadable(lazy(() => import("../pages/auth/ResetPassword")));
-const NewPasswordPage = Loadable(lazy(() => import("../pages/auth/NewPassword")));
+const ResetPasswordPage = Loadable(
+    lazy(() => import("../pages/auth/ResetPassword"))
+);
+const NewPasswordPage = Loadable(
+    lazy(() => import("../pages/auth/NewPassword"))
+);
 const VerifyPage = Loadable(lazy(() => import("../pages/auth/Verify")));
 
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
